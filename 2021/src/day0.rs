5865ed37c83fa508e5174cf9a04e7991d47efe1b
Marcos Suarez <msrxse@gmail.com>
2023-08-11 16:09:20 +0100

    2021 day 1

--- 2021/src/day0.rs
+++ 2021/src/day0.rs
@@ -1,10 +1,10 @@
-const INPUT: &str = include_str!("0.txt");
+const INPUT: &str = include_str!("1.txt");
 
 pub fn part1() -> String {
     part1_inner(INPUT)
 }
 
-pub fn part1_inner(input: &str) -> String {
+pub fn part1_inner(_input: &str) -> String {
     "12345".to_owned()
 }
 
@@ -12,7 +12,7 @@ pub fn part2() -> String {
     part2_inner(INPUT)
 }
 
-pub fn part2_inner(input: &str) -> String {
+pub fn part2_inner(_input: &str) -> String {
     "12345".to_owned()
 }
 
@@ -21,6 +21,20 @@ fn test_part1() {
     assert_eq!(
         "1985",
         part1_inner(
+            "ULL
+                RRDDD
+                LURDL
+                UUUUD
+            "
+        )
+    );
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(
+        "1985",
+        part2_inner(
             "ULL
                 RRDDD
                 LURDL
--- 2021/src/day1.rs
+++ 2021/src/day1.rs
@@ -5,7 +5,19 @@ pub fn part1() -> String {
 }
 
 pub fn part1_inner(input: &str) -> String {
-    "12345".to_owned()
+    input
+        .lines()
+        .fold((0, None), |(mut n, prev), line| {
+            let depth = line.trim().parse::<i64>().unwrap();
+            if let Some(prev) = prev {
+                if depth > prev {
+                    n += 1;
+                }
+            }
+            (n, Some(depth))
+        })
+        .0
+        .to_string()
 }
 
 pub fn part2() -> String {
@@ -13,19 +25,37 @@ pub fn part2() -> String {
 }
 
 pub fn part2_inner(input: &str) -> String {
-    "12345".to_owned()
+    input
+        .lines()
+        .collect::<Vec<&str>>()
+        .windows(3)
+        .fold((0, None), |(mut n, prev), lines| {
+            let depth = lines
+                .iter()
+                .fold(0, |sum, line| sum + line.trim().parse::<i64>().unwrap());
+            if let Some(prev) = prev {
+                if depth > prev {
+                    n += 1;
+                }
+            }
+            (n, Some(depth))
+        })
+        .0
+        .to_string()
 }
 
 #[test]
 fn test_part1() {
     assert_eq!(
-        "1985",
-        part1_inner(
-            "ULL
-                RRDDD
-                LURDL
-                UUUUD
-            "
-        )
+        "7",
+        part1_inner("199\n200\n208\n210\n200\n207\n240\n269\n260\n263")
+    );
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(
+        "5",
+        part2_inner("199\n200\n208\n210\n200\n207\n240\n269\n260\n263")
     );
 }
