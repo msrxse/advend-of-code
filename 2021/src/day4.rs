07afe6559ba2dfb92cbb06396b396f2a1805a828
Marcos Suarez <msrxse@gmail.com>
2023-08-29 15:13:49 +0100

    2021 day 2

--- 2021/src/day4.rs
+++ 2021/src/day4.rs
@@ -90,7 +90,47 @@ pub fn part2() -> String {
 }
 
 pub fn part2_inner(input: &str) -> String {
-    "12345".to_owned()
+    let mut lines = input.lines();
+    let draws = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|num| num.trim().parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+    let mut boards = Vec::new();
+    let mut next_board = Vec::new();
+    for line in lines {
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+        next_board.push(
+            line.split_ascii_whitespace()
+                .map(|num| (num.trim().parse::<i32>().unwrap(), false))
+                .collect::<Vec<_>>(),
+        );
+        if next_board.len() == 5 {
+            boards.push(next_board);
+            next_board = Vec::new();
+        }
+    }
+    let mut final_score = None;
+    for draw in draws {
+        boards = boards
+            .into_iter()
+            .fold(Vec::new(), |mut boards, mut board| {
+                if let Some(score) = play(&mut board, draw) {
+                    final_score = Some((draw * score).to_string());
+                } else {
+                    boards.push(board);
+                }
+                boards
+            });
+        if boards.is_empty() {
+            return final_score.unwrap();
+        }
+    }
+    panic!("no winner");
 }
 
 #[test]
@@ -124,13 +164,27 @@ fn test_part1() {
 #[test]
 fn test_part2() {
     assert_eq!(
-        "1985",
+        "1924",
         part2_inner(
-            "ULL
-                RRDDD
-                LURDL
-                UUUUD
-            "
+            "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+            22 13 17 11  0
+             8  2 23  4 24
+            21  9 14 16  7
+             6 10  3 18  5
+             1 12 20 15 19
+            
+             3 15  0  2 22
+             9 18 13 17  5
+            19  8  7 25 23
+            20 11 10 24  4
+            14 21 16 12  6
+            
+            14 21 17 24  4
+            10 16 15  9 19
+            18  8 23 26 20
+            22 11 13  6  5
+             2  0 12  3  7"
         )
     );
 }
